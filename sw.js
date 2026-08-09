9ccd5145a4b1be9f4ccb1d65466ef02595722136
ricardocarvajalnavarrete-commits <ricardo.carvajal.navarrete@gmail.com>
2026-08-08 21:52:21 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE='billetera-v1';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./icon.svg','./manifest.webmanifest'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET'||!e.request.url.startsWith(self.location.origin))return;
+  e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(r=>r||fetch(e.request).then(res=>{
+    const cl=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cl));return res;
+  }).catch(()=>caches.match('./index.html'))));
+});
